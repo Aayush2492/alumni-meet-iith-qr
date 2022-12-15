b0f3c2642f6b84b41cd6c6ebf95d54b0aa9e1235
Aayush2492 <aayushpatel2402@gmail.com>
2022-12-15 19:46:14 +0530

    Layout changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
 export default function Layout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}{" "}
+      <footer className={styles.footer}>
+        <a href="https://iith.dev" target="_blank" rel="noopener noreferrer">
+          A{" "}
+          <span className={styles.logo}>
+            <Image
+              src="/lambda_logo.png"
+              alt="Lambda Logo"
+              width={20}
+              height={20}
+            />
+          </span>
+          &nbsp; Production
+        </a>
+      </footer>
+    </>
+  );
 }
